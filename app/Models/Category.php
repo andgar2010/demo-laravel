fcabb09509e5e808fc9505722af1c8c676f703cf
Andrés García <afgarcia0479@misena.edu.co>
2018-12-06 18:03:39 -0500

    [WIP]

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Models;
-
+use App\Models\Restaurant;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+
+    public function restaurants()
+    {
+        return $this->hasMany(Restaurant::class);
+    }
 }
