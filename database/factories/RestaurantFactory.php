173e65f18f3d75ae0391b5bfd9a2fe346b7efcac
Andrés García <afgarcia0479@misena.edu.co>
2018-12-06 14:00:18 -0500

    Factory to DB

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Models\Category;
 use Faker\Generator as Faker;
+
 
 $factory->define(App\Models\Restaurant::class, function (Faker $faker) {
     return [
+        'name' => $faker->name,
+        'category_id'=> function(){
+            return Category::all()->random();
+        },
+        'description' => $faker->word,
+        'telephone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'schedule' => $faker->dateTime,
+        'description' => $faker->word,
+        'img_path' => $faker->imageUrl($width = 640, $height = 480),
         //
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\Models\Category::class,20)->create();
+
+        factory(App\Models\Restaurant::class, 120)->create();
     }
 }
